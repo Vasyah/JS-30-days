08a7e210bf676d248d6a687dc02c22276e387c05
Vasyah <Souji_933@mail.ru>
2020-10-09 23:31:34 +0700

    task #4

--- my/cart/scripts/index.js
+++ my/cart/scripts/index.js
@@ -7,27 +7,10 @@
 // самые частые события
 // click
 // mousemove
-let cart = {
-    items: {},
-    loadItems(items){
-        items.reduce((prev,item) => {
-            this.items[`item${item.dataset.id}`]= {};
-        },{});
-    },
-};
+// 
 // addToCart
 // добавить текущий товар в корзину
-let d = document;
-let cartTotaSumm = d.querySelector('.cart__totalSumm');
-let cards = d.querySelectorAll('.card__item');
-function addToCart() {
-    console.log(cartTotaSumm.value++);
-}
-cart.loadItems(cards);
-console.log(cart);
-let btnAdd = d.querySelectorAll('.btn-add');
-
-btnAdd.forEach(btn => btn.addEventListener('click', addToCart));
+// 
 // delFromCart
 // удалить выбранный товар из корзины
 
@@ -46,3 +29,36 @@ btnAdd.forEach(btn => btn.addEventListener('click', addToCart));
 
 // closeCart
 // закрыть корзину
+
+
+// let cart = {
+    //     items: {},
+    //     loadItems(items){
+    //         items.reduce((prev,item) => {
+    //             this.items[`item${item.dataset.id}`]= {};
+    //         },{});
+    //     },
+    // };
+
+let arr = [1, 3, 4];
+
+arr.filter(currItem => currItem >= 3? console.log(currItem): '');
+
+// let d = document;
+// let cartTotaSumm = d.querySelector('.cart__totalSumm');
+// let cards = d.querySelectorAll('.card__item');
+// function addToCart() {
+//     console.log(cartTotaSumm.value++);
+// }
+// cart.loadItems(cards);
+// console.log(cart);
+// let btnAdd = d.querySelectorAll('.btn-add');
+
+// btnAdd.forEach(btn => btn.addEventListener('click', addToCart));
+
+// Для Полины задание 12.10.20
+// Самостоятельно выполнить предыдущее задание
+// реализовать все функции две функции из задания и использовать методы массивов
+
+// Для Юли задание 12.10
+// Решить задания с использованием методов массивов
