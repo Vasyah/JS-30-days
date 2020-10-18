76e19301612bec90f875ff472179762bbf226581
Vasyah <Souji_933@mail.ru>
2020-10-18 17:42:32 +0700

    add del delAll fnc added

--- my/cart/build/main.07544d9b.js
+++ my/cart/build/main.07544d9b.js
@@ -117,84 +117,79 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"scripts/cart.js":[function(require,module,exports) {
-'use strict';
+})({"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-var cart = {};
-var d = document;
-var products = d.querySelector('.products');
-var productsInputs = d.querySelectorAll('.product__count'); // добавить в корзину
-
-function addCart(id, price) {
-  // лежит ли уже в корзине
-  if (cart.hasOwnProperty(id)) {
-    cart[id].count += 1;
-    cart[id]['totalPrice'] = cart[id]['count'] * cart[id]['price'];
-  } // если не лежит
-  else {
-      cart[id] = {};
-      cart[id].count = 1;
-      cart[id]['price'] = +price;
-      cart[id]['totalPrice'] = cart[id]['count'] * cart[id]['price'];
-    }
-
-  console.log(cart);
-}
-
-; // удалить из корзины
-
-function delCart(id) {
-  // если не осталось товара в корзине, удалить из объекта
-  if (cart.hasOwnProperty(id)) {
-    if (cart[id]['count'] - 1 === 0) {
-      delete cart[id];
-      console.log(cart);
-      return;
-    }
-
-    cart[id].count -= 1;
-    cart[id]['totalPrice'] = cart[id]['count'] * cart[id]['price'];
-  } // если не лежит
-  else {
-      console.log(cart);
-      return;
-    }
-
-  console.log(cart);
-}
-
-;
-
-function delAllCart() {
-  for (var item in cart) {
-    delete cart[item];
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
   }
 
-  console.log(cart);
+  return bundleURL;
 }
 
-function changeCount(e, id) {
-  if (!cart.hasOwnProperty(id)) return;
-  e.target.parentNode.children[1].value = cart[e.path[2].dataset.id]['count'];
-}
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
 
-products.addEventListener('click', function (e) {
-  // нажата кнопка "+" ?
-  if (e.target.classList.contains('product__btn--plus')) {
-    addCart(e.path[2].dataset.id, e.path[2].dataset.price);
-    changeCount(e, e.path[2].dataset.id);
-  } // нажата кнопка "-" ?
-  else if (e.target.classList.contains('product__btn--minus')) {
-      delCart(e.path[2].dataset.id);
-      changeCount(e, e.path[2].dataset.id);
-    } else if (e.target.classList.contains('products__btn--clear')) {
-      delAllCart();
-      productsInputs.forEach(function (i) {
-        return i.value = 0;
-      });
+    if (matches) {
+      return getBaseURL(matches[0]);
     }
-});
-},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"sass/main.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./..\\img\\shopping-cart.svg":[["shopping-cart.293e2283.svg","img/shopping-cart.svg"],"img/shopping-cart.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -398,5 +393,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","scripts/cart.js"], null)
-//# sourceMappingURL=/cart.8a7a0a92.js.map
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/main.07544d9b.js.map
--- my/cart/scripts/cart.js
+++ my/cart/scripts/cart.js
@@ -1,22 +1,72 @@
-// let cart = {
-//     items: {},
-//     addToCart(item){
-//         if(this.hasOwnProperty(this.currentItem[item])){
-//             this.currentItem[item]['quantity'] += 1;
-//         }
-//         else{
-//             this.currentItem[item] = 1;
-//         }
-//     }
-// }
+'use strict'
+let cart = {}
 
-// let items = {
-//     'id-1': {
-//         price: '450',
-//     },
-//     'id-2': {
-//         price: '450',
-//     },
-// }
+let d = document;
+const products = d.querySelector('.products');
+const productsInputs = d.querySelectorAll('.product__count');
+// добавить в корзину
+function addCart(id, price) {
+    // лежит ли уже в корзине
+    if (cart.hasOwnProperty(id)) {
+        cart[id].count += 1;
+        cart[id]['totalPrice'] = cart[id]['count'] * cart[id]['price'];
+    }
+    // если не лежит
+    else {
+        cart[id] = {};
+        cart[id].count = 1;
+        cart[id]['price'] = +price;
+        cart[id]['totalPrice'] = cart[id]['count'] * cart[id]['price'];
+    }
+    console.log(cart);
+};
 
-// console.log(items);
+// удалить из корзины
+function delCart(id) {
+    // если не осталось товара в корзине, удалить из объекта
+    if (cart.hasOwnProperty(id)) {
+        if (cart[id]['count'] - 1 === 0) {
+            delete cart[id];
+            console.log(cart);
+            return;
+        }
+        cart[id].count -= 1;
+        cart[id]['totalPrice'] = cart[id]['count'] * cart[id]['price'];
+    }
+    // если не лежит
+    else {
+        console.log(cart);
+        return;
+    }
+    console.log(cart);
+};
+
+function delAllCart() {
+    for (let item in cart) {
+        delete cart[item];
+    }
+    console.log(cart);
+}
+
+function changeCount(e, id) {
+    if (!cart.hasOwnProperty(id)) return;
+    e.target.parentNode.children[1].value = cart[e.path[2].dataset.id]['count'];
+}
+
+
+products.addEventListener('click', e => {
+    // нажата кнопка "+" ?
+    if (e.target.classList.contains('product__btn--plus')) {
+        addCart(e.path[2].dataset.id, e.path[2].dataset.price);
+        changeCount(e, e.path[2].dataset.id);
+    }
+    // нажата кнопка "-" ?
+    else if (e.target.classList.contains('product__btn--minus')) {
+        delCart(e.path[2].dataset.id);
+        changeCount(e, e.path[2].dataset.id);
+    } 
+    else if (e.target.classList.contains('products__btn--clear')) {
+        delAllCart();
+        productsInputs.forEach( i => i.value = 0);
+    }
+});
